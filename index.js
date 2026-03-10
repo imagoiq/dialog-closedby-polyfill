6457d4761f982c0aa2646bd56b1d5ca815e85063
imagoiq <12294151+imagoiq@users.noreply.github.com>
2026-03-10 11:33:19 +0100

    fix: feature detection on Safari 26.3

--- index.js
+++ index.js
@@ -209,7 +209,6 @@ function isSupported() {
   }
   try {
     const testDialog = document.createElement("dialog");
-    testDialog.setAttribute("closedby", "none");
     return testDialog.closedBy === "none";
   } catch {
     return false;
